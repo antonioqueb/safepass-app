13a02b240bf88bcaf3c666663718b1523e6c838b
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-02-20 13:38:21 +0000

    Add templates

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask, jsonify, request
-import os
+from flask import Flask, render_template
 import secrets
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html', password=secrets.token_hex(16))
+
+@app.route('/generate-password')
 def generate_password():
     password = secrets.token_hex(16)
-    return jsonify({'password': password})
+    return {'password': password}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
