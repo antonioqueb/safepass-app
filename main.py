e18126334ab8867db7ec834db4bc51d350f5bb94
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-02-20 07:22:57 -0600

    "add json-generate-password"

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
+import secrets
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+def generate_password():
+    password = secrets.token_hex(16)
+    return jsonify({'password': password})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
